962b0da5324121eac75afa5dec32f5d82e94fde5
brian gerson <brianpgerson@gmail.com>
2016-01-25 17:00:13 -0800

    implements public api

--- first_routes/bin/my_script.rb
+++ first_routes/bin/my_script.rb
@@ -6,10 +6,12 @@ url = Addressable::URI.new(
   scheme: 'http',
   host: 'localhost',
   port: 3000,
-  path: '/users.md'
+  path: '/users/10.json'
 ).to_s
 begin
-  puts RestClient.get(url)
+  puts RestClient.patch(url,
+    {user: { username: "idonwannawait" }}
+  )
 rescue RestClient::Exception
   p "WHOOPS!!"
 end
