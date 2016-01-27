554a7c5bce8db147c24aa891fb5bb7eef77c3c53
App Academy Student <student@appacademy.io>
2016-01-26 18:01:55 -0800

    implements entire cat rental system

--- ninetynine_cats/app/models/cat.rb
+++ ninetynine_cats/app/models/cat.rb
@@ -1,12 +1,15 @@
 class Cat < ActiveRecord::Base
-  COLORS = [:white, :black, :brown, :calico, :orange]
+  COLORS = ['white', 'black', 'brown', 'calico', 'orange']
 
-  validates_presence_of :cat_tables, :birthdate, :color, :name, :sex, :description
+  validates_presence_of :birthdate, :color, :name, :sex, :description
   validates :color, inclusion: { in: COLORS }
-  validates :sex, inclusion: { in: [:m, :f] }
+  validates :sex, inclusion: { in: ['m', 'f'] } # %w(m f)
+
+  has_many :cat_rental_requests, dependent: :destroy
 
   private
   def age
     (Date.today - self.birthdate).to_i/365
   end
+
 end
