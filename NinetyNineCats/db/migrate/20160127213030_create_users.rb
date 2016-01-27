967846a29bbec8d8ff233e4f2fa5bca12808b80c
App Academy Student <student@appacademy.io>
2016-01-27 14:13:14 -0800

    User sets session token and password

---
+++ NinetyNineCats/db/migrate/20160127213030_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :user_name, null: false
+      t.string :password_digest, null: false
+    end
+      add_index :users, :user_name, unique: true
+      add_index :users, :password_digest
+  end
+end
