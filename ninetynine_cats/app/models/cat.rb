d98374ee8012d3cc6e122c4dc20cc2af4123747c
brian gerson <brianpgerson@gmail.com>
2016-01-27 19:01:34 -0800

    dogs comments

--- ninetynine_cats/app/models/cat.rb
+++ ninetynine_cats/app/models/cat.rb
@@ -6,7 +6,7 @@ class Cat < ActiveRecord::Base
   validates :sex, inclusion: { in: ['m', 'f'] } # %w(m f)
 
   has_many :cat_rental_requests, dependent: :destroy
-
+  #dogs dogs dogs
   private
   def age
     (Date.today - self.birthdate).to_i/365
