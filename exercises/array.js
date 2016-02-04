adbe0b5cac57888485ef3cff8d354639386a6787
brian gerson <brianpgerson@gmail.com>
2016-02-04 15:28:06 -0800

    recursion is fun in javascript if recurs.a,..

--- exercises/array.js
+++ exercises/array.js
@@ -1,66 +1,82 @@
-function uniq(array) {
+Array.prototype.uniq = function () {
     var uniqueElements = [];
-    for (var i = 0; i < array.length; i++) {
-      if (uniqueElements.indexOf(array[i]) === -1) {
-        uniqueElements.push(array[i]);
+    for (var i = 0; i < this.length; i++) {
+      if (uniqueElements.indexOf(this[i]) === -1) {
+        uniqueElements.push(this[i]);
       }
     }
     return uniqueElements;
-}
+};
 
-function twoSum(array) {
+Array.prototype.twoSum = function () {
   var sumPairs = [];
-  for (var i = 0; i < array.length; i++) {
-    for (var j = i + 1; j < array.length; j++) {
-      if (array[i] + array[j] === 0) {
+  for (var i = 0; i < this.length; i++) {
+    for (var j = i + 1; j < this.length; j++) {
+      if (this[i] + this[j] === 0) {
         sumPairs.push([i, j]);
       }
     }
   }
   return sumPairs;
-}
+};
 
-function transpose(array){
+Array.prototype.transpose = function () {
   var transposed = [];
-  for (var rowNum = 0; rowNum < array.length; rowNum++) {
+  for (var rowNum = 0; rowNum < this.length; rowNum++) {
     var newRow = [];
-    for (var colNum = 0; colNum < array[rowNum].length; colNum++) {
-      newRow.push(array[colNum][rowNum]);
+    for (var colNum = 0; colNum < this[rowNum].length; colNum++) {
+      newRow.push(this[colNum][rowNum]);
     }
     transposed.push(newRow);
   }
   return transposed;
-}
+};
 
-// function other_transpose(array){
-//   // TODO predefined array size?
-//   // var transposed = new Array(array[0].length,(new Array(array.length),[]))
-//   for (var i = 0; i < array.length; i++) {
-//     for (var j = 0; j < array[0].length; j++) {
-//       transposed[i][j] = array[j][i]
-//     }
-//   }
-// }
-
-function myEach(array, myFun) {
-  for (var i = 0; i < array.length; i++) {
-    myFun(array[i]);
+Array.prototype.myEach = function (myFun) {
+  for (var i = 0; i < this.length; i++) {
+    myFun(this[i]);
   }
-}
+};
 
 
-function myMap(array, mapFunc) {
+Array.prototype.myMap = function (mapFunc) {
   var newArray = [];
-  myEach(array, function(el) {
+  this.myEach(function(el) {
     newArray.push(mapFunc(el));
   });
   return newArray;
-}
+};
 
-function myInject(array, injectFunc){
-  var accumulator = array.shift();
-  myEach(array, function(el){
+Array.prototype.myInject = function (injectFunc){
+  var accumulator = this.shift();
+  this.myEach(function(el){
     accumulator = injectFunc(accumulator, el);
   });
   return accumulator;
+};
+
+Array.prototype.bubbleSort = function () {
+  var swapped = true;
+  while (swapped) {
+    swapped = false;
+    for (var i = 0; i < this.length - 1; i++) {
+      if (this[i] > this[i + 1]) {
+        swapped = true;
+        var temp = this[i];
+        this[i] = this[i + 1];
+        this[i + 1] = temp;
+      }
+    }
+  }
+  return this;
+};
+
+function substrings(string) {
+  var substrs = [];
+  for (var i = 0; i < string.length - 1; i++) {
+    for (var j = i + 1; j <= string.length; j++) {
+      substrs.push(string.slice(i, j));
+    }
+  }
+  return substrs.uniq;
 }
