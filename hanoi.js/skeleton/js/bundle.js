3a1670b3fe30863e0a8ee8c1be77294c5bb197a2
brian gerson <brianpgerson@gmail.com>
2016-02-10 15:34:05 -0800

    implement setup towers

--- hanoi.js/skeleton/js/bundle.js
+++ hanoi.js/skeleton/js/bundle.js
@@ -44,7 +44,7 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var HanoiView = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./view.js\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	var HanoiView = __webpack_require__(1);
 	var HanoiGame =
 	__webpack_require__(2);
 	
@@ -56,7 +56,47 @@
 
 
 /***/ },
-/* 1 */,
+/* 1 */
+/***/ function(module, exports) {
+
+	function View(game, $el) {
+	  this.game = game;
+	  this.$el = $el;
+	  this.setupTowers();
+	}
+	
+	View.prototype.setupTowers = function () {
+	  for (var i = 0; i < 3; i++) {
+	    var $tower = $("<ul>").data('towerNum', i);
+	    for (var j = 0; j < 3; j++) {
+	      var $disk = $("<li>");
+	      $tower.append($disk);
+	    }
+	    this.$el.append($tower);
+	  }
+	  this.updateClasses();
+	};
+	
+	View.prototype.updateClasses = function () {
+	  for (var n = 0; n < this.game.towers.length; n++) {
+	    for (var k = 0; k < this.game.towers[n].length; k++) {
+	      debugger;
+	      var diskSize = this.game.towers[n][k];
+	      var className = "disk-" + diskSize;
+	      var $tower = $(this.$el.find('ul')[n]);
+	      var $disks = $($tower.find('li'));
+	      var $disk = $([].slice.apply($disks).reverse()[k]);
+	      $disk.addClass(className);
+	    }
+	  }
+	
+	};
+	
+	
+	module.exports = View;
+
+
+/***/ },
 /* 2 */
 /***/ function(module, exports) {
 
--- hanoi.js/skeleton/js/view.js
+++ hanoi.js/skeleton/js/view.js
@@ -8,16 +8,28 @@ View.prototype.setupTowers = function () {
   for (var i = 0; i < 3; i++) {
     var $tower = $("<ul>").data('towerNum', i);
     for (var j = 0; j < 3; j++) {
-      var $disc = $("<li>");
-      $tower.append($disc);
+      var $disk = $("<li>");
+      $tower.append($disk);
     }
     this.$el.append($tower);
   }
-  for (var n = 0; n < this.game.towers.length; n++) {
-    for (var k = 0; k < this.game.towers[i].length; k++)
+  this.updateClasses();
+};
 
+View.prototype.updateClasses = function () {
+  for (var n = 0; n < this.game.towers.length; n++) {
+    for (var k = 0; k < this.game.towers[n].length; k++) {
+      debugger;
+      var diskSize = this.game.towers[n][k];
+      var className = "disk-" + diskSize;
+      var $tower = $(this.$el.find('ul')[n]);
+      var $disks = $($tower.find('li'));
+      var $disk = $([].slice.apply($disks).reverse()[k]);
+      $disk.addClass(className);
     }
   }
+
 };
-hello
+
+
 module.exports = View;
