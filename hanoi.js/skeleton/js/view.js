b46b08bb5c12fab6a178b7ebf45f5791aa93d40e
Carson Swope <carsonmswope@gmail.com>
2016-02-10 16:16:50 -0800

    implement craaAAAaaaAAazy hillary background

--- hanoi.js/skeleton/js/view.js
+++ hanoi.js/skeleton/js/view.js
@@ -1,7 +1,9 @@
 function View(game, $el) {
   this.game = game;
   this.$el = $el;
+  this.currentPile = undefined;
   this.setupTowers();
+  this.bindStuff();
 }
 
 View.prototype.setupTowers = function () {
@@ -17,19 +19,45 @@ View.prototype.setupTowers = function () {
 };
 
 View.prototype.updateClasses = function () {
+
+  var $towers = $(this.$el.find('ul'));
+
   for (var n = 0; n < this.game.towers.length; n++) {
+
+    var $tower = $($towers[n]);
+    var $disks = $($tower.find('li'));
+    $disks.removeClass('disk-1 disk-2 disk-3');
+
     for (var k = 0; k < this.game.towers[n].length; k++) {
-      debugger;
       var diskSize = this.game.towers[n][k];
       var className = "disk-" + diskSize;
-      var $tower = $(this.$el.find('ul')[n]);
-      var $disks = $($tower.find('li'));
       var $disk = $([].slice.apply($disks).reverse()[k]);
       $disk.addClass(className);
     }
   }
-
 };
 
+View.prototype.bindStuff = function () {
+  this.$el.on('click', 'ul', this.clickTower.bind(this));
+};
+
+View.prototype.clickTower = function(e) {
+  if (this.currentPile === undefined) {
+
+    this.currentPile = $(e.currentTarget).data('towerNum');
+    $(this.$el.find('ul')[this.currentPile]).toggleClass('selected');
+
+  } else {
+    try {
+      this.game.move(this.currentPile, $(e.currentTarget).data('towerNum'));
+    } catch(err) {
+      alert("Whatcha doin' there, hombre?");
+    }
+    $(this.$el.find('ul')[this.currentPile]).toggleClass('selected');
+    this.currentPile = undefined;
+  }
+
+  this.updateClasses();
+};
 
 module.exports = View;
